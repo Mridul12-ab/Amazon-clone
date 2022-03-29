3cba16ea1a62a6b57028888a0185b35132abf998
Mridul_5 <mahajanmridul258@gmail.com>
2022-03-29 16:36:50 +0530

    Initial commit-2

--- src/App.js
+++ src/App.js
@@ -3,11 +3,17 @@ import './App.css';
 import Header from './Header';
 import Home from './Home';
 import Checkout from './Checkout';
+import Payment from './Payment';
 //import {Switch} from "react-router";
 import { BrowserRouter as Router, Switch, Route} from "react-router-dom"; 
 import Login from './Login';
 import { auth } from './firebase1';
 import { useStateValue } from './StateProvider';
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
+
+const promise = loadStripe(
+  'pk_test_51KfLiSSJKulUw9ubN53sssEZBwI8gJ4F5Q9nxroZ9ayZNFKrLumsjBxA4Lw77lydXt7UTVcsqHVklevEI60QCgcZ00zAMyfamF');
 
 function App() {
     const[{}, dispatch] = useStateValue();
@@ -46,6 +52,12 @@ function App() {
               <Route path="/checkout">
                   <Header/>
                  <Checkout/>
+              </Route>
+              <Route path="/payment">
+                 <Header />
+                 <Elements stripe={promise}>
+                    <Payment />
+                 </Elements>
               </Route>
               <Route path="/">
                   <Header/>
